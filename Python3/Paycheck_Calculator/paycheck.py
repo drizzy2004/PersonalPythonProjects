21884ee9aa44cea870e240906d5f93c972f4b977
drizzy2004 <157053650+drizzy2004@users.noreply.github.com>
2024-10-30 14:38:11 -0600

    added investment display to paycheck calc

--- Python3/Paycheck_Calculator/paycheck.py
+++ Python3/Paycheck_Calculator/paycheck.py
@@ -5,13 +5,15 @@ def calc_paycheck(hours):
     tax_chunk = hours * hourly_wage * 0.173
     investment_chunk = hours * hourly_wage * 0.08
 
+    print(f"Investment: ${investment_chunk:.2f}")
     return (hours * hourly_wage) - tax_chunk - investment_chunk
+
 
 
 if __name__ == "__main__":
     hours = int(sys.argv[1])
     total = calc_paycheck(hours)
 
-    print(f"${total:.2f}")
+    print(f"Check: ${total:.2f}")
 
 
